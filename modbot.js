e5fe7fef0ad4ac22c00aa098075a2d4c55b4d3c2
Rebecca Turner <me@re-becca.org>
2018-11-10 16:36:24 -0800

    chore: standardize

--- modbot.js
+++ modbot.js
@@ -11,18 +11,18 @@ const TOML = require('@iarna/toml')
 
 // @iarna/cli above will run this when the process starts
 async function main (opts, conffile) {
-  const bot = await DiscoBot.create(Object.assign({emoji: {report: 'report'}}, TOML.parse(fs.readFileSync(conffile))))
+  const bot = await DiscoBot.create(Object.assign({ emoji: { report: 'report' } }, TOML.parse(fs.readFileSync(conffile))))
   bot.addCommand('report', {
     usage: 'report <reason...>',
     description: 'report inappropriate activity',
     filter: $ => $.server,
-    action: async function ($, {reason}) {
+    action: async function ($, { reason }) {
       const chname = ($.msg.channel && $.msg.channel.type !== 'dm') ? $.msg.channel : 'DM'
       console.log(`**REPORT** from ${this.name($.msg.author)} in ${this.name(chname)}: ${reason.join(' ')}`)
       if ($.server.deleteReports && $.msg.channel.name) $.msg.delete().catch(() => {})
-      await $.server.moderation.send(`@here **REPORT** from ${$.msg.author} in ${chname}: ${reason.join(' ')}`, {split: true})
+      await $.server.moderation.send(`@here **REPORT** from ${$.msg.author} in ${chname}: ${reason.join(' ')}`, { split: true })
       if ($.server.deleteReports && $.msg.channel.name) {
-        return this.sendDM($.msg.author, `Report in ${chname} has been sent to moderators: ${reason.join(' ')}`, {split: true})
+        return this.sendDM($.msg.author, `Report in ${chname} has been sent to moderators: ${reason.join(' ')}`, { split: true })
       } else {
         return $.msg.react($.server.emoji.report)
       }
@@ -32,13 +32,13 @@ async function main (opts, conffile) {
     description: 'message the admins',
     usage: 'admin <message...>',
     filter: $ => $.server,
-    action: async function ($, {message}) {
+    action: async function ($, { message }) {
       const chname = ($.msg.channel && $.msg.channel.type !== 'dm') ? $.msg.channel : 'DM'
       console.log(`Message to admins from ${this.name($.msg.author)} in ${this.name(chname)}: ${message.join(' ')}`)
       if ($.server.deleteReports && $.msg.channel.name) $.msg.delete().catch(() => {})
-      await $.server.moderation.send(`Message to admins from ${$.msg.author} in ${chname}: ${message.join(' ')}`, {split: true})
+      await $.server.moderation.send(`Message to admins from ${$.msg.author} in ${chname}: ${message.join(' ')}`, { split: true })
       if ($.server.deleteReports && $.msg.channel.name) {
-        return this.sendDM($.msg.author, `Mesage to admins in ${chname} has been sent: ${message.join(' ')}`, {split: true})
+        return this.sendDM($.msg.author, `Mesage to admins in ${chname} has been sent: ${message.join(' ')}`, { split: true })
       } else {
         return $.msg.react('✅')
       }
